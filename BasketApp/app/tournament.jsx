7df9819454e92d9c5dd39d8c883af4ed7981d0d9
ComputerVis35 <viponnusamy2@gmail.com>
2025-01-27 10:47:30 -0500

    added a tournament generation landing page

---
+++ BasketApp/app/tournament.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const tournament = () => {
+    const [tournamentData] = useState(null);
+    return (
+        <View>
+            <Text>Tournament Generation</Text>
+        </View>
+    )
+}
+
+export default tournament;
