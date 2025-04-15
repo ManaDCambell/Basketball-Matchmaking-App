b80124ba175447c812422fe6a66ee5afe47fff21
ManaDCambell <143769675+ManaDCambell@users.noreply.github.com>
2025-04-15 18:38:00 -0400

    Update FirebaseConfig.ts
    Removed api key

--- BasketApp/FirebaseConfig.ts
+++ BasketApp/FirebaseConfig.ts
@@ -8,18 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+//firebase config goes here
+
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAUO8FDKV1iqsk_PaACNsBWu-I1NfRTfa4",
-  authDomain: "basketapp-a14bb.firebaseapp.com",
-  projectId: "basketapp-a14bb",
-  storageBucket: "basketapp-a14bb.appspot.com",
-  messagingSenderId: "219787223865",
-  appId: "1:219787223865:web:c2dc20cfcef29114837ee8",
-  measurementId: "G-E02WPNCYZE"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
